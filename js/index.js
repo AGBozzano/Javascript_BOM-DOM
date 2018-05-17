e65d48869d42e3ca2fcdb5285223e855eb0aebd8
AGBozzano <ale.kpo.mdq@gmail.com>
2018-05-17 00:51:23 -0300

    aplicacion v1.2

---
+++ js/index.js
@@ -0,0 +1,59 @@
+var Registro = [
+    {
+        "codigo": "001",
+        "nombre": "Alejandro",
+        "nota": 4
+    }, {
+        "codigo": "002",
+        "nombre": "Andres",
+        "nota": 3
+    }, {
+        "codigo": "003",
+        "nombre": "Federico",
+        "nota": 5
+    }, {
+        "codigo": "004",
+        "nombre": "Gonzalo",
+        "nota": 8
+    }, {
+        "codigo": "005",
+        "nombre": "Matias",
+        "nota": 3
+    }, {
+        "codigo": "006",
+        "nombre": "Cristian",
+        "nota": 8
+    }
+];
+function leerJSON(json) {
+    var out = "-----------Ventas-----------<br>";
+    var i;
+    for (i = 0; i < json.length; i++) {
+        out += "Codigo:" + json[i].codigo + "-" + "Valor:" + json[i].valor + "<br>";
+    }
+
+    document.getElementById("ventas").innerHTML = out;
+
+}
+function calcularTotalVentas(json) {
+
+    var out = "-----------Total En Ventas-----------<br>";
+    var totalVentas = 0.0;
+    for (i = 0; i < json.length; i++) {
+        totalVentas += json[i].valor;
+    }
+
+ 
+
+    document.getElementById("totalVentas").innerHTML = "El total de venta es de : " + totalVentas;
+
+}
+function mostrarVentas() {
+    leerJSON(ventas);
+}
+
+
+function mostrarTotalVentas() {
+    calcularTotalVentas(ventas);
+}
+
